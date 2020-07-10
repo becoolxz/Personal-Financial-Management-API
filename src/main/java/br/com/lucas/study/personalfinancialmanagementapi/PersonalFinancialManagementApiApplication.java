63e48205193b3f551d4ca084c025434a75bb2cac
Lucas Ribeiro <tianoxz.lr@gmail.com>
2020-07-10 09:30:39 -0300

    Created initial structure of project.

---
+++ src/main/java/br/com/lucas/study/personalfinancialmanagementapi/PersonalFinancialManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.study.personalfinancialmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancialManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancialManagementApiApplication.class, args);
+	}
+
+}
